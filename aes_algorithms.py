a630f18d7d903dce3aba19c55f81430e404e6b86
Baloch-Rasheed <balochrasheed888@gmail.com>
2025-11-19 10:57:45 +0500

    AES with CBC Mode

--- aes_algorithms.py
+++ aes_algorithms.py
@@ -1,18 +1,23 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from base64 import b64decode, b64encode
+from Crypto.Random import get_random_bytes
 
 class AES_Alg:
 
     def encrypt(self, key, message):
+        iv = get_random_bytes(16)
         padded_message = pad(message.encode(), AES.block_size)
-        aes_cipher = AES.new(key.encode(), AES.MODE_ECB)
+        aes_cipher = AES.new(key.encode(), AES.MODE_CBC, iv)
         cipher_bytes = aes_cipher.encrypt(padded_message)
-        return b64encode(cipher_bytes)
+        return b64encode(iv + cipher_bytes)
     
     def decrypt(self, key, cipher_text):
-        aes_cipher = AES.new(key.encode(), AES.MODE_ECB)
-        decoded_cipher_text = b64decode(cipher_text)
+        cipher_text = b64decode(cipher_text)
+        iv = cipher_text[:16]
+        cipher_text = cipher_text[16:]
+        aes_cipher = AES.new(key.encode(), AES.MODE_CBC, iv)
+        decoded_cipher_text = cipher_text
         plain_bytes = aes_cipher.decrypt(decoded_cipher_text)
         plain_text = unpad(plain_bytes, AES.block_size)
         return plain_text.decode()
